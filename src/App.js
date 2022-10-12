162452530dddb4aaf0aaae490751935cac4a7b7c
joymozumder <joy.mozumder.1003@gmail.com>
2022-10-12 22:31:12 +0600

    added statistics component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layouts/Main';
 import Home from './components/Home/Home';
 import Quiz from './components/Quiz/Quiz';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
   const router = createBrowserRouter([
@@ -22,6 +23,11 @@ function App() {
           loader: async ({params}) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.quizId}`),
           element: <Quiz></Quiz>
         },
+        {
+          path: "statistics",
+          loader: async () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Statistics></Statistics>
+        }
       ]
     },
   ]);
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Statistics.css';
+const Statistics = () => {
+    const topics = useLoaderData().data;
+    return (
+        <Container>
+        <div className='d-flex justify-content-center my-5'>
+        <ResponsiveContainer width="100%" height={500}>
+          <BarChart
+            width={700}
+            height={500}
+            data={topics}
+            
+          >
+            <CartesianGrid/>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total" fill="#8884d8" />
+          </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </Container>
+    );
+};
+
+export default Statistics;
