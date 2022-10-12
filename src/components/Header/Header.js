6b4af116b10b668d23c65acd47bc1d0e129849fa
joymozumder <joy.mozumder.1003@gmail.com>
2022-10-12 23:30:22 +0600

    added margin in link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,10 +12,10 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2' to="/">Home</Link>
-                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2' to="/">Topics</Link>
-                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2' to="/statistics">Statistics</Link>
-                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2' to="/blog">Blog</Link>
+                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2 my-1 text-center' to="/">Home</Link>
+                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2 my-1 text-center' to="/">Topics</Link>
+                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2 my-1 text-center' to="/statistics">Statistics</Link>
+                    <Link className='me-3 custom-nav-item  text-decoration-none px-3 py-2  text-white rounded-2 my-1 text-center' to="/blog">Blog</Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
