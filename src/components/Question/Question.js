5e971845d85c7722748e524b64a4cfe9c480365d
joymozumder <joy.mozumder.1003@gmail.com>
2022-10-12 22:00:40 +0600

    added toast in question component

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,14 +1,37 @@
 import React, { useState } from 'react';
-import { Card, ListGroup } from 'react-bootstrap';
+import { Card, ListGroup, Toast } from 'react-bootstrap';
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 import './Question.css';
 
 const Question = ({ quizQuestion, questionNumber }) => {
     const { question, options, correctAnswer, id } = quizQuestion;
+    const [answer, setAnswer] = useState(false);
+    const [show, setShow] = useState(false);
     const [showAnswer, setShowAnswer] = useState(false);
+    console.log(questionNumber, question);
+    const checkAnswer = (e) => {
+        console.log(e);
+        if (e.target.value === correctAnswer) {
+        setAnswer(true);
+        } else {
+        setAnswer(false);
+        }
+    };
     
     return (
         <div>
+            <Toast
+                className="ms-auto"
+                onClose={() => setShow(false)}
+                show={show}
+                delay={8000}
+                autohide>
+                <Toast.Header className={`${answer ? "bg-success" : "bg-danger"}`}>
+                <strong className="me-auto text-white">
+                    {answer ? "Right" : "Wrong"} Answer !!!
+                </strong>
+                </Toast.Header>
+            </Toast>
             <Card className="mb-2">
                 <Card.Header className="d-flex justify-content-between">
                     <p className="py-3">
@@ -30,7 +53,11 @@ const Question = ({ quizQuestion, questionNumber }) => {
                         id={`group${questionNumber}-${indx}`}
                         name={`group${questionNumber}`}
                         value={option}
-                        
+                        onClick={(e) => {
+                            console.log(e);
+                            checkAnswer(e);
+                            setShow(true);
+                          }}
                     ></input>
                     <label
                         className="ms-2"
